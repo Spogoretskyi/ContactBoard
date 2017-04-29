585047581fd5b1c52d3e084f6d08e59bd0e26421
leonid korniienko <leo@pdffiller.com>
2017-04-29 14:24:38 +0300

    add query database method

--- config/db.php
+++ config/db.php
@@ -5,12 +5,25 @@ class db
     protected $host;
     protected $user;
     protected $password;
+    protected $connection;
+    protected $database_name;
 
     public function __construct()
     {
         $this->host = 'localhost';
         $this->user = 'homestead';
         $this->password = 'secret';
+        $this->connect();
+    }
+
+    public function connect()
+    {
+        $this->connection = new mysqli($this->host, $this->user, $this->password, $this->database_name);
+    }
+
+    public function query($query)
+    {
+        return mysqli_query($this->connection, $query);
     }
 
 
