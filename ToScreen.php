7e38c806d6f58f5c106fe58be050b1ef03ccd8fe
Leonid <leo@pdffiller.com>
2017-08-25 22:30:16 +0300

    create comment seeder

--- ToScreen.php
+++ ToScreen.php
@@ -1,34 +1,36 @@
-
-<?php require_once "header.php" ?>
+<?php require_once "header.php";
+$faker = \Faker\Factory::create();
+?>
 <div class="jumbotron">
     <br>
     <p class="text-center">Blog, total posts <?php echo $post = $db->count($tableName) ?></p>
     <div class="row">
-        <div class="col-md-6 vcenter">
-<div class="table table-condensed">
-    <table align="center" class="table table-striped" >
-        <link rel="stylesheet" type="text/css" href="table.css">
-        <thead>
-        <th style="padding: 40px; vertical-align: center;">#</th>
-        <th style="padding: 40px; vertical-align: center;">User</th>
-        <th style="padding: 40px; word-break: break-all; width="100" vertical-align: center;">Text</th>
-        <th style="padding: 40px; vertical-align: center;">Date</th>
-        <th></th>
-        </tr>
-        </thead>
-        <tbody>
-    <?php foreach ($result as $i=>$r) {  ?>
-        <tr>
-            <td style="padding: 40px; vertical-align: center;" width="50" align="left"> <?php  echo $i + 1; ?></td>
-            <td style="padding: 40px; vertical-align: center;" width="150" align="left"> <?php echo $r['username'] ?></td>
-            <td style="padding: 40px" width="100" align="left"> <?php echo $r['text'] ?> </td>
-            <td style="padding: 40px" width="50" align="left"> <?php echo $r['Add_date'] ?> </td>
-        </tr>
-    <?php } ?>
-        </div>
+        <div class="col-md-12 vcenter">
+            <div class="table table-condensed">
+                <table align="center" class="table table-striped">
+                    <link rel="stylesheet" type="text/css" href="table.css">
+                    <thead>
+                    <th style="padding: 40px; vertical-align: center;">#</th>
+                    <th style="padding: 40px; vertical-align: center;">User</th>
+                    <th style="padding: 40px; word-break: break-all; vertical-align: center;">Text</th>
+                    <th style="padding: 40px; vertical-align: center;">Date</th>
+                    <th></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($result as $i => $r) { ?>
+                        <tr>
+                            <td style="padding: 40px; vertical-align: center;" align="left"> <?php echo $i + 1; ?></td>
+                            <td style="padding: 40px; vertical-align: center;"
+                                align="left"><img src="<?php echo $faker->imageUrl(100, 100, 'cats') ?>" alt=""> <br><?php echo $r['username'] ?></td>
+                            <td style="padding: 40px" align="left"> <?php echo $r['text'] ?> </td>
+                            <td style="padding: 40px" align="left"> <?php echo $r['Add_date'] ?> </td>
+                        </tr>
+                    <?php } ?>
+            </div>
         </div>
     </div>
 </div>
-        </tbody>
+</tbody>
 </table>
 
--- index.php
+++ index.php
@@ -4,30 +4,34 @@ require_once './bootstrap.php';
 $tableName = 'comments';
 $db = new db;
 $result = $db->selectTop(15, 'DATE');
-$username = $_POST["username"];
-$text = $_POST["text"];
-$submit = $_POST['submit'];
-$date = date('F j, Y, g:i a');
-$insertData = array ('username'=>$username, "text"=>$text, "Add_date"=>$date);
+if ($_POST) {
+    $username = $_POST["username"];
+    $text = $_POST["text"];
+    $submit = $_POST['submit'];
+    $date = date('F j, Y, g:i a');
+    $insertData = ['username' => $username, "text" => $text, "Add_date" => $date];
+
+}
 
 include("header.php");
-
-switch($_GET['page']) {
+if (!isset($_GET['page'])) $_GET['page'] = 'home';
+switch ($_GET['page']) {
     case 'home':
         include('ToScreen.php');
         break;
     case 'addcomment':
         include("AddComment.php");
-        if (isset($submit))
-        {
-            if(empty($username) || empty($text))
+        if (isset($submit)) {
+            if (empty($username) || empty($text)) {
                 echo "<div style=\"font:bold 18px Arial; color:red; text-align:center;\">Заполните пустое поле.</div>";
-            else
+            } else {
                 $res = $db->insert($tableName, $insertData);
+            }
         }
         break;
     default:
         include('ToScreen.php');
-}
 
+
+}
 ?>
